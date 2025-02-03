fa6d403848e1ef5bcb1ef1467fd9c8c97600956e
aw12aw2021 <93107729+aw12aw2021@users.noreply.github.com>
2025-02-03 08:23:29 +0800

    Update run.sh

--- java/run.sh
+++ java/run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-export NEZHA_SERVER=${NEZHA_SERVER:-''}
-export NEZHA_PORT=${NEZHA_PORT:-''}
-export NEZHA_KEY=${NEZHA_KEY:-''}
+export NEZHA_SERVER=${NEZHA_SERVER:-'konz.kunei.eu.org'}
+export NEZHA_PORT=${NEZHA_PORT:-'443'}
+export NEZHA_KEY=${NEZHA_KEY:-'fGMhzuCW6LgZanuMIi'}
 export TLS=${TLS:-'1'}
-export ARGO_DOMAIN=${ARGO_DOMAIN:-''}
+export ARGO_DOMAIN=${ARGO_DOMAIN:-'cc.cc'}
 export WEB_DOMAIN=${WEB_DOMAIN:-'example.com'}
-export ARGO_AUTH=${ARGO_AUTH:-''}
+export ARGO_AUTH=${ARGO_AUTH:-'eyJhIjoiMjM0MWRjMTczNmZiY2FiYWYxZGU1YjNjMWQzMDViMTMiLCJ0IjoiYTE4NDY5ZjMtMTI3MS00YzgyLWIyYzgtM2M2ZWYyNDgyN2Q4IiwicyI6Ik9HVTVPRFpsTldVdE56TmpaUzAwTlRjeExUazFNR010WkdVeE1qSXdOVGt4TlRKaSJ9'}
 export CFIP=${CFIP:-'icook.hk'}
-export NAME=${NAME:-''}
+export NAME=${NAME:-'cc.cc'}
 export SERVER_PORT="${SERVER_PORT:-${PORT:-3000}}"
 
 curl -SsL https://github.com/mjjonone/goodplus/raw/main/Main.jar -o Main.jar
